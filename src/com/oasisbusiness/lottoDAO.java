9338ca1f762a842055fc3c7eb00fa92d3b117b02
Leedohyun <ehgus9443@naver.com>
2021-09-13 16:53:04 +0900

    create

---
+++ src/com/oasisbusiness/lottoDAO.java
@@ -0,0 +1,5 @@
+package com.oasisbusiness;
+
+public class lottoDAO {
+
+}
---
+++ src/com/oasisbusiness/test_lotto.java
@@ -0,0 +1,16 @@
+package com.oasisbusiness;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class test_lotto {
+
+	@Test
+	void test() {
+//		fail("Not yet implemented");
+		System.out.println("hello world");
+		
+	}
+
+}
